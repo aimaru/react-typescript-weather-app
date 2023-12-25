239aaa0cba7f0736d0093f1355dfddb129137aee
Aiko M <minmilera@gmail.com>
2023-12-25 23:30:25 +0100

    Add Title and Form components

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Title from "./components/Title";
+import Form from "./components/Form";
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="app">
+      <Title/>
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
